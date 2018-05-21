24b1414f9ffefbecf326108d299874cd498f8a9c
mikspe <msperkowski@airmail.cc>
2018-05-21 15:33:46 +0200

    initial commit

---
+++ World.java
@@ -0,0 +1,21 @@
+public class World
+{
+	private int width;
+	private int height;
+	private int idArray[][];
+	private Organism organisms[];
+
+	private void fillIdArray()
+	{}
+
+	private void sortOrganisms()
+	{}
+
+	private void randSpawn()
+	{}
+
+	private void drawWorld(int turnNumber)
+	{}
+
+	 
+}
